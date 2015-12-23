b1cf1892c58679f73e7b9d2741aa130fd2f63cdb
Tharnid <jeffmsmithdev@gmail.com>
2015-12-22 23:13:23 -0500

    on to bower

--- Node/Library/app.js
+++ Node/Library/app.js
@@ -4,6 +4,17 @@ var app = express();
 
 var port = 1974;
 
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+
+app.get('/', function(req, res){
+    res.send('Hello Route!!!');
+});
+
+app.get('/books', function(req, res) {
+   res.send('Hello Books!!!'); 
+});
+
 app.listen(1974, function(err) {
     console.log('Server running on port ' + port + '!!!');
 });

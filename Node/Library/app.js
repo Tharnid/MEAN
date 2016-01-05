89735ab524673cf34b311eec9714b10c134ef859
Tharnid <jeffmsmithdev@gmail.com>
2016-01-05 14:51:17 -0500

    moving along now

--- Node/Library/app.js
+++ Node/Library/app.js
@@ -17,8 +17,21 @@ app.set('views', './src/views');
 
 app.set('view engine', 'ejs');
 
+//app.get('/', function (req, res) {
+//    res.render('index', {title: 'Hello EJS and Tharnbrary!!!', list: ['Han', 'Lando', 'Chewy']});
+//});
+
 app.get('/', function (req, res) {
-    res.render('index', {title: 'Hello EJS and Tharnbrary!!!', list: ['Han', 'Lando', 'Chewy']});
+    res.render('index', {
+        title: 'Hello from render',
+        nav: [{
+            Link: '/Books',
+            Text: 'Books'
+        }, {
+            Link: '/Authors',
+            Text: 'Authors'
+        }]
+    });
 });
 
 //app.get('/', function (req, res) {

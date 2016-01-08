ff58577f47d2174626ffc21a627b3590be3eeb6d
Tharnid <jeffmsmithdev@gmail.com>
2016-01-08 11:12:45 -0500

    on to add a single book

--- Node/Library/src/routes/bookRoutes.js
+++ Node/Library/src/routes/bookRoutes.js
@@ -1,52 +1,28 @@
 var express = require('express');
-
 var bookRouter = express.Router();
+var mongodb = require('mongodb').MongoClient;
 
-var books = [
-    {
-        title: 'Phoenix in Action',
-        genre: 'Programming',
-        author: 'Jason Terrell',
-        read: false
-    },
-    {
-        title: 'Elixir in Practice',
-        genre: 'Programming',
-        author: 'Tharnid Saldor',
-        read: false
-    },
-    {
-        title: 'Phoenix in Practice',
-        genre: 'Programming',
-        author: 'Jason Smith',
-        read: false
-    },
-    {
-        title: 'Elm in Action',
-        genre: 'Programming',
-        author: 'Dawn Nelson',
-        read: false
-    }
-];
+// mongo cli
+// show collections
+// db.books.find();
 
 // Routing
 var router = function(nav) {
     bookRouter.route('/')
     .get(function (req, res) {
-        res.render('bookListView', {
-            title: 'Books',
-            nav: nav,
-            books: books
-        });
-    });
-
-    bookRouter.route('/:id')
-    .get(function (req, res) {
-        var id = req.params.id;
-        res.render('bookView', {
-            title: 'Books',
-            nav: nav,
-            book: books[id]
+        var url =
+            'mongodb://localhost:27017/tharnbraryApp';
+        mongodb.connect(url, function (err, db) {
+            var collection = db.collection('books');
+            collection.find({}).toArray(
+                function(err, results) {
+                    res.render('bookListView', {
+                        title: 'Books',
+                        nav: nav,
+                        books: results
+                    });
+                }
+            );
         });
     });
     return bookRouter;

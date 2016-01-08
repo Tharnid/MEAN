41dc70e2b2e016324d3258d0c8ea793d5fa79c4f
Tharnid <jeffmsmithdev@gmail.com>
2016-01-08 00:03:12 -0500

    moving on to adding books

--- Node/Library/app.js
+++ Node/Library/app.js
@@ -14,6 +14,7 @@ var nav = [{
             }
           ];
 
+var adminRouter = require('./src/routes/adminRoutes')(nav);
 var bookRouter = require('./src/routes/bookRoutes')(nav);
 
 app.use(express.static('public'));
@@ -22,6 +23,7 @@ app.set('views', './src/views');
 app.set('view engine', 'ejs');
 
 app.use('/Books', bookRouter);
+app.use('/Admin', adminRouter);
 
 // Index
 

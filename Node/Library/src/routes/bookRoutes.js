a0ec64df2e83a05addac81cbe309d7abb3cdb57b
Tharnid <jeffmsmithdev@gmail.com>
2016-01-08 12:20:47 -0500

    hooked up a single book view

--- Node/Library/src/routes/bookRoutes.js
+++ Node/Library/src/routes/bookRoutes.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var bookRouter = express.Router();
 var mongodb = require('mongodb').MongoClient;
+var objectId = require('mongodb').ObjectID;
 
 // mongo cli
 // show collections
@@ -25,6 +26,31 @@ var router = function(nav) {
             );
         });
     });
+    bookRouter.route('/:id')
+        .get(function (req, res) {
+            var id = new objectId(req.params.id);
+            var url =
+                'mongodb://localhost:27017/tharnbraryApp';
+
+            mongodb.connect(url, function (err, db) {
+                var collection = db.collection('books');
+
+                collection.findOne({
+                        _id: id
+                    },
+                    function (err, results) {
+                        res.render('bookView', {
+                            title: 'Books',
+                            nav: nav,
+                            book: results
+                        });
+
+                    }
+                );
+
+            });
+
+        });
     return bookRouter;
 };
 

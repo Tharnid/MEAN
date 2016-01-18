982baaf482849b1cc33d05b9b5b4be181287a6d7
Tharnid <jeffmsmithdev@gmail.com>
2016-01-18 18:23:46 -0500

    well my stuff was hosed after adding HTTP

--- Node/Tharnbrary/src/services/goodreadsService.js
+++ Node/Tharnbrary/src/services/goodreadsService.js
@@ -1,8 +1,33 @@
+var http = require('http');
+var xml2js = require('xml2js');
+var parser = xml2js.Parser({explicitArray: false});
 
 var goodreadsService = function() {
 
     var getBookById = function(id, cb) {
-        cb(null, {description: 'Your bleeping description'});
+        
+        var options = function(id, cb) {
+            host: 'www.goodreads.com',
+            path: '/book/show/656?format=xml&key=A3ZwnoAlQSudKqDHWXgVYg'
+        };       
+        var callback = function(response) {
+            var str = '';
+            
+            response.on('data', function(chunk) {
+                str += chunk;
+            });
+            response.on('end', function() {
+                console.log(str);
+                parser.parseString(str,
+                    function(err, result) {
+                        cb(null, 
+                           result.GoodreadsResponse.book);
+                });
+            });
+        };
+//        cb(null, {description: 'Your bleeping description'});
+        
+        http.request(options, callback).end();
     };
 
     return {

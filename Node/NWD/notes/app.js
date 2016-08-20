53d231029f3aa84f3f182ed932997c7cdb101065
Tharnid <jeffmsmithdev@gmail.com>
2016-08-20 15:50:44 -0400

    implemented notes stored in filesystem using json

--- Node/NWD/notes/app.js
+++ Node/NWD/notes/app.js
@@ -1,42 +1,24 @@
+'use strict';
+
+var util = require('util');
 var express = require('express');
 var path = require('path');
+var fs   = require('fs');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
+var FileStreamRotator = require('file-stream-rotator');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var FileStreamRotator = require('file-stream-rotator');
+
+var routes = require('./routes/index');
+// var users = require('./routes/users');
+var notes  = require('./routes/notes');
+
 var error = require('debug')('notes:error');
 
 process.on('uncaughtException', function(err) {
-  error("I've crashed!!! - " + (err.stack || err));
+  error("I've crashed!!! - "+ (err.stack || err));
 });
-
-if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
-    // util.log(err.message);
-    res.status(err.status || 500);
-    error((err.status || 500) +' '+ error.message);
-    res.render('error', {
-      message: err.message,
-      error: err
-    });
-  });
-}
-
-var accessLogStream;
-if (process.env.REQUEST_LOG_FILE) {
-  var logDirectory = path.dirname(process.env.REQUEST_LOG_FILE);
-  fs.existsSync(logDirectory)  || fs.mkdirSync(logDirectory);
-  accessLogStream = FileStreamRotator.getStream({
-    filename: process.env.REQUEST_LOG_FILE,
-    frequency: 'daily',
-    verbose: false
-  });
-}
-
-var routes = require('./routes/index');
-var notes = require('./routes/notes');
-// var users = require('./routes/users');
 
 var app = express();
 
@@ -44,23 +26,27 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+var accessLogStream;
+if (process.env.REQUEST_LOG_FILE) {
+  var logDirectory = path.dirname(process.env.REQUEST_LOG_FILE);
+  fs.existsSync(logDirectory) || fs.mkdirSync(logDirectory);
+  accessLogStream = FileStreamRotator.getStream({
+    filename: process.env.REQUEST_LOG_FILE,
+    frequency: 'daily',
+    verbose: false
+  });
+}
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-//app.use(logger('dev'));
-//app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev'));
 app.use(logger(process.env.REQUEST_LOG_FORMAT || 'dev', {
-  steam: accessLogStream ? accessLogStream : process.stdout
+  stream: accessLogStream ? accessLogStream : process.stdout
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// app.use('../bower_components/bootstrap/dist/css/bootstrap.min.css', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist', 'css')));
-// app.use('../bower_components/bootstrap/dist/fonts', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist', 'fonts')));
-// app.use('../bower_components/bootstrap/dist//js', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist', 'js')));
-// app.use('../bower_components/jquery/dist/jquery', express.static(path.join(__dirname, 'bower_components', 'jquery', 'dist')));
-
+// app.use('/vendor/bootstrap/css', express.static(path.join(__dirname, 'cyborg')));
 app.use('/vendor/bootstrap/css', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist', 'css')));
 app.use('/vendor/bootstrap/fonts', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist', 'fonts')));
 app.use('/vendor/bootstrap/js', express.static(path.join(__dirname, 'bower_components', 'bootstrap', 'dist', 'js')));
@@ -68,18 +54,7 @@ app.use('/vendor/jquery', express.static(path.join(__dirname, 'bower_components'
 
 app.use('/', routes);
 // app.use('/users', users);
-app.use('/notes', notes)
-
-// Uncaught Exceptions
-app.use(function(err, req, res, next) {
-  // util.log(err.message);
-  res.status(err.status || 500);
-  error((err.status || 500) +' '+ error.message);
-  res.render('error', {
-    message: err.message,
-    error: {}
-  });
-});
+app.use('/notes', notes);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -94,7 +69,9 @@ app.use(function(req, res, next) {
 // will print stacktrace
 if (app.get('env') === 'development') {
   app.use(function(err, req, res, next) {
+    // util.log(err.message);
     res.status(err.status || 500);
+    error((err.status || 500) +' '+ error.message);
     res.render('error', {
       message: err.message,
       error: err
@@ -105,7 +82,9 @@ if (app.get('env') === 'development') {
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
+  // util.log(err.message);
   res.status(err.status || 500);
+  error((err.status || 500) +' '+ error.message);
   res.render('error', {
     message: err.message,
     error: {}
--- Node/NWD/notes/routes/index.js
+++ Node/NWD/notes/routes/index.js
@@ -1,27 +1,28 @@
 'use strict';
 
 var util = require('util');
+var path = require('path');
 var express = require('express');
 var router = express.Router();
-var notes = require('../models/notes-memory');
+var notes = require(process.env.NOTES_MODEL ? path.join('..', process.env.NOTES_MODEL) : '../models/notes-memory');
+
+const log   = require('debug')('notes:router-home');
+const error = require('debug')('notes:error');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     notes.keylist()
         .then(keylist => {
-        var keyPromises = [];
-    for (var key of keylist) {
-        keyPromises.push(
-            notes.read(key)
-                .then(note => {
-                return { key: note.key, title: note.title };
-    })
-    );
-    }
+        var keyPromises = keylist.map(key => {
+                return notes.read(key).then(note => {
+                    return { key: note.key, title: note.title };
+});
+});
     return Promise.all(keyPromises);
 })
     .then(notelist => {
-        res.render('index', {
+        log('HOMEPAGE notelist='+ util.inspect(notelist));
+    res.render('index', {
         title: 'Notes',
         notelist: notelist,
         breadcrumbs: [
@@ -29,7 +30,7 @@ router.get('/', function(req, res, next) {
         ]
     });
 })
-    .catch(err => { next(err); });
+    .catch(err => { error(err); next(err); });
 });
 
 module.exports = router;
